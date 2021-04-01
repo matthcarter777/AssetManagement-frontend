fc547958fd28c8f513a101b0cd65805fc4f13e5b
Mateus <mateushbsb@gmail.com>
2021-04-01 12:13:54 -0300

    validate user form

--- src/app/components/user/user-create/user-create.component.ts
+++ src/app/components/user/user-create/user-create.component.ts
@@ -2,6 +2,7 @@ import { User } from './../../../models/user.model';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-create',
@@ -17,22 +18,47 @@ export class UserCreateComponent implements OnInit {
     password: ''
   }
 
+  
+  name = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required]);
+  cpf = new FormControl('', [Validators.required]);
+  matricula = new FormControl('', [Validators.required]);
+
   constructor(
     private userService: UserService,
     private router: Router
   ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   create(): void {
-    this.userService.create(this.user).subscribe(() => {
-      this.userService.showMessage('Usuario criado com sucesso!');
-      this.router.navigate(['/users']);
-    })
+    if(
+      this.user.name === '' ||
+      this.user.cpf === ''  ||
+      this.user.registration === '' ||
+      this.user.email === ''
+    ) {
+      this.getErrorMessage()
+      this.userService.showMessage('Algum campo obrigatorio vazio!');
+    } else {
+
+      this.userService.create(this.user).subscribe(() => {
+        this.userService.showMessage('Usuario criado com sucesso!');
+        this.router.navigate(['/users']);
+      })
+    }
+
   }
 
   cancel(): void {
     this.router.navigate(['/users'])
   }
+  
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'campo não pode ser vazio.';
+    } 
+    return
+  }
+
 }
